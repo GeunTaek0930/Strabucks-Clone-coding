ce62ed93aea3446a518bef05ef43c1b14c39c9dd
이근택 <101080311+GeunTaek0930@users.noreply.github.com>
2022-05-16 00:03:04 +0900

    2022-05-16 스벅 최종

--- 스타벅스/js/main.js
+++ 스타벅스/js/main.js
@@ -17,26 +17,43 @@ searchInputEl.addEventListener('blur', function () {
 
 
 
-const badgeEl = document.querySelector(`header  .badges`);
-
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener('scroll', _.throttle(function () {
-  console.log(window.scrollY);
+  // 페이지 스크롤 위치가 500px이 넘으면.
   if (window.scrollY > 500) {
-    // 배지 숨기기
-    // gsap.to(요소, 지속시간, 옵션);
+    // Badge 요소 숨기기!
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
-    });
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+
+    // 페이지 스크롤 위치가 500px이 넘지 않으면.
   } else {
-    // 배지 보이기
+    // Badge 요소 보이기!
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
-    });
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
   }
-}, 300));
-//  _.throttle(함수,시간)
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
